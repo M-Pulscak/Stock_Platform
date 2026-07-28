db191e26a9bf9c56b797ca28b3b58eeaea33b279
M-Pulscak <pumi666@atlas.cz>
2026-07-28 12:03:31 +0200

    Update

--- src/services/universe_importer.py
+++ src/services/universe_importer.py
@@ -3,6 +3,7 @@ from models.universe import Universe
 from providers.universe.universe_provider import UniverseProvider
 from repositories.universe_member_repository import UniverseMemberRepository
 from repositories.universe_repository import UniverseRepository
+from utils.logger import get_logger
 
 
 class UniverseImporter:
@@ -16,25 +17,32 @@ class UniverseImporter:
         self.db = db
         self.universe = universe
         self.provider = provider
-
         self.universe_repo = UniverseRepository(db)
         self.member_repo = UniverseMemberRepository(db)
+        self.logger = get_logger("UniverseImporter")
 
     def import_universe(self) -> Universe:
-
-        # vytvoří univerzum, pokud ještě neexistuje
+        self.logger.info("=" * 60)
+        self.logger.info(
+            "Importing universe %s (%s)",
+            self.universe.code,
+            self.universe.provider,
+        )
+        self.logger.info("=" * 60)
         universe = self.universe_repo.get_or_create(self.universe)
-
         if universe.universe_id is None:
             raise RuntimeError("Universe ID was not assigned.")
-
-        # načte aktuální členy od providera
+        self.logger.info("Downloading members...")
         members = self.provider.get_members()
-
-        # kompletně nahradí obsah univerza
+        self.logger.info("Downloaded %d members.", len(members))
+        self.logger.info("Synchronizing database...")
         self.member_repo.replace_members(
             universe.universe_id,
             members,
         )
-
+        self.logger.info(
+            "Universe %s synchronized (%d members).",
+            universe.code,
+            len(members),
+        )
         return universe
